ddebc859d04fd723324dbbcba1cc5da728522311
Weewml <annasmir89@gmail.com>
2025-04-26 19:47:34 +0300

    controller

--- src/main/java/com/example/ProjectsShowcase/controllers/MainController.java
+++ src/main/java/com/example/ProjectsShowcase/controllers/MainController.java
@@ -1,14 +1,19 @@
 package com.example.ProjectsShowcase.controllers;
 
+import com.example.ProjectsShowcase.models.MyUser;
+import com.example.ProjectsShowcase.models.ProjectFullInfo;
+import com.example.ProjectsShowcase.models.Team;
+import jakarta.websocket.server.PathParam;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.ProjectsShowcase.repositories.UserRepository;
 import com.example.ProjectsShowcase.services.Parser;
 import com.example.ProjectsShowcase.repositories.ProjectRepository;
 import com.example.ProjectsShowcase.repositories.TeamRepository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +30,80 @@ public class MainController {
         parser.loadData();
         return "saved";
     }
+
+
+    // все проекты
+    @GetMapping("/projects")
+    public Iterable<ProjectFullInfo> findAllProject() {
+        return projectRepository.findAll();
+    }
+
+    // информация о проекте
+    @GetMapping("/projects/{id}")
+    public ProjectFullInfo getProjectInfo(@PathVariable Long id) {
+        return projectRepository.findById(id).get();
+    }
+
+
+    // все пользователи
+    @GetMapping("/users")
+    public Iterable<MyUser> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    // информация о пользователе
+    @GetMapping("/users/{id}")
+    public MyUser getUserInfo(@PathVariable Long id) {
+        return userRepository.findById(id).get();
+    }
+
+
+    // все команды
+    @GetMapping("/teams")
+    public Iterable<Team> getAllTeam() {
+        return teamRepository.findAll();
+    }
+
+    // информация о команде - по айди пользователя
+    @GetMapping("/teams/{id}")
+    public Team getTeamInfo(@PathVariable Long id) {
+        return teamRepository.findByTeammates_id(id);
+    }
+
+    // создание команды
+    @PostMapping("/teams/add/{id}/{name}")
+    public String createTeam(@PathVariable Long id, @PathVariable String name,
+                             @RequestParam(required = false) List<Long> teammatesId) {
+
+        MyUser teamlid = userRepository.findById(id).get();
+        Team team = new Team(null, name, teamlid, List.of(teamlid),
+                null, null, null);
+
+        teamRepository.save(team);
+        return "saved";
+    }
+
+    // бронирование проекта (current project)
+    @PostMapping("/projects/add/{id}/{teamlidId}")
+    public String addCurrentProject(@PathVariable Long id, @PathVariable Long teamlidId) {
+
+        teamRepository.findByTeammates_id(teamlidId)
+                .setCurrentProject(projectRepository.findById(id).get());
+
+        teamRepository.save(teamRepository.findByTeammates_id(teamlidId));
+        return "saved project";
+    }
+
+    // удаление проекта из current project в refuse project
+    @PostMapping("/projects/refuse/{teamlidId}")
+    public String delCurrentProject(@PathVariable Long teamlidId) {
+
+        teamRepository.findByTeammates_id(teamlidId)
+                .refuseProject("конец");
+
+        teamRepository.save(teamRepository.findByTeammates_id(teamlidId));
+        return "delete current project";
+    }
+
+
 }
